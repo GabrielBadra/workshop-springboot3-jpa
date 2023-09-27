e28055105a264bab2816a274fa66274034daec42
GabrielBadra <gabrielfreitasalves9@gmail.com>
2023-09-27 16:43:43 -0300

    Exception handling - findById

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> entity = entityRepository.findById(id);
-		return entity.get();
+		return entity.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User entity) {
@@ -33,9 +34,9 @@ public class UserService {
 	}
 	
 	//ESSE ID É O ID DO OBJETO USER QUE QUER ATUALIZAR
-	public User uptade(Long id, User entity) {
+	public User uptade(Long id, User obj) {
 		User entity = entityRepository.getReferenceById(id);
-		uptadeData(entity, entity);
+		uptadeData(entity, obj);
 		return entityRepository.save(entity);
 	}
 	
