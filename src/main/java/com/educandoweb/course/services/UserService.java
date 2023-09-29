715c08c36b46d72e0925fd13125b771a3db405f6
GabrielBadra <gabrielfreitasalves9@gmail.com>
2023-09-29 16:09:51 -0300

    Exception handling - uptade

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -13,42 +13,48 @@ import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository entityRepository;
-	
+
 	public List<User> findAll() {
 		return entityRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> entity = entityRepository.findById(id);
 		return entity.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User entity) {
 		return entityRepository.save(entity);
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			entityRepository.deleteById(id);
-		}catch(EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
-	//ESSE ID É O ID DO OBJETO USER QUE QUER ATUALIZAR
+
+	// ESSE ID É O ID DO OBJETO USER QUE QUER ATUALIZAR
 	public User uptade(Long id, User obj) {
-		User entity = entityRepository.getReferenceById(id);
-		uptadeData(entity, obj);
-		return entityRepository.save(entity);
+		try {
+			User entity = entityRepository.getReferenceById(id);
+			uptadeData(entity, obj);
+			return entityRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
-	
+
 	private void uptadeData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
